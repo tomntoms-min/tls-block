b41fdb0bf3180f972fa6722b34ccaaccf71a7f63
tomntoms-min <tomntoms@korea.ac.kr>
2025-06-11 21:37:24 +0900

    Update tlshdr.h

--- tlshdr.h
+++ tlshdr.h
@@ -1,12 +1,24 @@
 #pragma once
+
 #include <cstdint>
 
-// TLS 레코드의 필수적인 앞부분에 쉽게 접근하기 위한 구조체
 #pragma pack(push, 1)
-struct Tls {
-    uint8_t tls_content;   // 22 (0x16) for Handshake
-    uint16_t tls_version;  // e.g., 0x0303 for TLS 1.2
-    uint16_t tls_length;   // 뒤따르는 데이터의 길이 (Network Byte Order)
-    uint8_t handshake_type; // 1 (0x01) for Client Hello
+
+// TLS Record Protocol
+struct TlsHdr {
+    uint8_t  content_type_;
+    uint16_t version_;
+    uint16_t length_;
+
+    enum : uint8_t {
+        Handshake = 22,
+    };
 };
+
+// TLS Handshake Protocol
+struct HandshakeHdr {
+    uint8_t  handshake_type_;
+    uint8_t  length_[3]; // 24-bit length
+};
+
 #pragma pack(pop)
