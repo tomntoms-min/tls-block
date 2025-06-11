eec14c58673c1763e7c27930f21e8b9d24737d7f
tomntoms-min <tomntoms@korea.ac.kr>
2025-06-11 20:11:52 +0900

    Update iphdr.h

--- iphdr.h
+++ iphdr.h
@@ -5,6 +5,7 @@
 
 #pragma pack(push, 1)
 struct IpHdr final {
+    // 사용자가 제공한 비트필드 구조
     uint8_t ip_len:4;
     uint8_t ip_v:4;
     uint8_t tos;
@@ -18,15 +19,15 @@ struct IpHdr final {
     uint8_t ttl;
     uint8_t proto;
     uint16_t check;
-    Ip sip_; // 타입이 Ip 객체임
-    Ip dip_; // 타입이 Ip 객체임
+    Ip sip_;
+    Ip dip_;
 
-    // [수정 1] const 추가
-    // [수정 2] Ip 객체이므로 ntohl 불필요, 그대로 반환
+    // [수정 1] const 추가: 이 함수는 멤버 변수를 변경하지 않음을 명시
+    // [수정 2] ntohl 제거: sip_와 dip_는 이미 호스트 바이트 순서의 Ip 객체이므로 변환 불필요
     Ip sip() const { return sip_; }
 	Ip dip() const { return dip_; }
 
-    // [추가] 헤더 길이를 반환하는 함수 (ip_len * 4)
+    // [추가] 가독성을 위해 IP 헤더 길이를 반환하는 헬퍼 함수 추가
     uint8_t ipHdrLen() const { return ip_len * 4; }
 
     // enum은 그대로 유지
