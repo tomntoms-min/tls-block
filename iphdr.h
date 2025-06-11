8cd43ad7c2b81c82eb65c0f6dd61376764960601
tomntoms-min <tomntoms@korea.ac.kr>
2025-06-11 20:11:22 +0900

    Update iphdr.h

--- iphdr.h
+++ iphdr.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <cstdint>
 #include <arpa/inet.h>
 #include "ip.h"
@@ -11,19 +10,30 @@ struct IpHdr final {
     uint8_t tos;
     uint16_t total_len;
     uint16_t id;
-    uint8_t frag_offset:5;
+    uint8_t frag_offset_1:5;
     uint8_t more_fragment:1;
     uint8_t dont_fragment:1;
     uint8_t reserved_zero:1;
-    uint8_t frag_offset2;
+    uint8_t frag_offset_2;
     uint8_t ttl;
     uint8_t proto;
     uint16_t check;
-    Ip sip_;
-    Ip dip_;
+    Ip sip_; // 타입이 Ip 객체임
+    Ip dip_; // 타입이 Ip 객체임
 
-    Ip sip() { return ntohl(sip_); }
-	Ip dip() { return ntohl(dip_); }
+    // [수정 1] const 추가
+    // [수정 2] Ip 객체이므로 ntohl 불필요, 그대로 반환
+    Ip sip() const { return sip_; }
+	Ip dip() const { return dip_; }
+
+    // [추가] 헤더 길이를 반환하는 함수 (ip_len * 4)
+    uint8_t ipHdrLen() const { return ip_len * 4; }
+
+    // enum은 그대로 유지
+    enum: uint8_t {
+        ICMP = 1,
+        TCP = 6,
+        UDP = 17,
+    };
 };
-typedef IpHdr* PIpHdr;
 #pragma pack(pop)
