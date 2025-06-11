4d5617a4c06ef5ce39edf358bdf7d7001b6893a6
tomntoms-min <tomntoms@korea.ac.kr>
2025-06-11 23:31:15 +0900

    Update packet_handler.h

--- packet_handler.h
+++ packet_handler.h
@@ -2,32 +2,50 @@
 
 #include <pcap.h>
 #include <string>
+#include <map>
 #include <vector>
-#include <cstdint>
+#include "ethhdr.h"
+#include "iphdr.h"
+#include "tcphdr.h"
+#include "mac.h"
+#include "ip.h"
 
-// 시스템 표준 헤더를 사용합니다.
-#include <net/ethernet.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
+// TCP 흐름을 식별하기 위한 4-tuple 구조체
+struct Flow {
+    Ip src_ip;
+    Ip dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+
+    bool operator<(const Flow& other) const;
+};
+
+// 조각화된 TCP 스트림의 상태를 저장하는 구조체
+struct Stream {
+    std::vector<uint8_t> data;
+    EthHdr eth_hdr;
+    IpHdr ip_hdr;
+    TcpHdr tcp_hdr;
+};
 
 class PacketHandler {
 public:
-    // 생성자에서 더 이상 Ip 클래스를 받지 않습니다.
-    PacketHandler(pcap_t* handle, uint8_t* my_mac, std::string server_name);
+    PacketHandler(pcap_t* handle, std::string iface, Mac my_mac, Ip my_ip, std::string server_name);
     void handlePacket(const struct pcap_pkthdr* header, const uint8_t* packet);
 
 private:
     pcap_t* pcap_handle_;
-    uint8_t my_mac_[6];
+    std::string interface_name_;
+    Mac my_mac_;
+    Ip my_ip_;
     std::string target_server_name_;
+    std::map<Flow, Stream> active_streams_;
+
+    // 함수 시그니처 수정
+    bool findSNIAndBlock(const EthHdr& eth_hdr, const IpHdr& ip_hdr, const TcpHdr& tcp_hdr, const uint8_t* tls_data, size_t tls_len);
+    void sendForwardRst(const EthHdr& eth_hdr, const IpHdr& ip_hdr, const TcpHdr& tcp_hdr, size_t payload_len);
+    void sendBackwardRst(const IpHdr& ip_hdr, const TcpHdr& tcp_hdr, size_t payload_len);
     
-    // SNI 파싱 함수
-    std::string parseSNI(const uint8_t* payload, int len);
-
-    // 패킷 전송 함수
-    void sendForwardRst(const uint8_t* orig_packet, const struct ip* ip_hdr, const struct tcphdr* tcp_hdr, int payload_len);
-    void sendBackwardRst(const struct ip* ip_hdr, const struct tcphdr* tcp_hdr, int payload_len);
-
-    // 체크섬 계산 함수
-    uint16_t calculateChecksum(uint16_t *buf, int nbytes);
+    static uint16_t calculateIpChecksum(IpHdr* ip_hdr);
+    static uint16_t calculateTcpChecksum(IpHdr* ip_hdr, TcpHdr* tcp_hdr, const uint8_t* data, size_t data_len);
 };
