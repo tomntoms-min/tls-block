ec19d08ae688c9c2f0cde5426f08d585e9139639
tomntoms-min <tomntoms@korea.ac.kr>
2025-06-11 20:06:55 +0900

    Update iphdr.h

--- iphdr.h
+++ iphdr.h
@@ -1,40 +1,29 @@
 #pragma once
 
-#include <netinet/in.h>
+#include <cstdint>
+#include <arpa/inet.h>
 #include "ip.h"
 
 #pragma pack(push, 1)
-
 struct IpHdr final {
-    uint8_t version_and_ihl;
-    uint8_t dscp_and_ecn;
-    uint16_t total_length;
-    uint16_t identification;
-    uint16_t flags_and_fragment_offset;
+    uint8_t ip_len:4;
+    uint8_t ip_v:4;
+    uint8_t tos;
+    uint16_t total_len;
+    uint16_t id;
+    uint8_t frag_offset:5;
+    uint8_t more_fragment:1;
+    uint8_t dont_fragment:1;
+    uint8_t reserved_zero:1;
+    uint8_t frag_offset2;
     uint8_t ttl;
-    uint8_t protocol;
-    uint16_t checksum;
-    uint32_t sip_;
-    uint32_t dip_;
+    uint8_t proto;
+    uint16_t check;
+    Ip sip_;
+    Ip dip_;
 
-    Ip sip() { return Ip(ntohl(sip_)); }
-    Ip dip() { return Ip(ntohl(dip_)); }
-
-    // Protocol(ip_protocol)
-    enum: uint8_t {
-        ICMP = 1,
-        IGMP = 2,
-        TCP = 6,
-        IGRP = 9,
-        UDP = 17,
-        GRE = 47,
-        ESP = 50,
-        AH = 51,
-        SKIP = 57,
-        EIGRP = 88,
-        OSPF = 89,
-        L2TP = 115
-    };
+    Ip sip() { return ntohl(sip_); }
+	Ip dip() { return ntohl(dip_); }
 };
-
+typedef IpHdr* PIpHdr;
 #pragma pack(pop)
