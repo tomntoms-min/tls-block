625399f1a6458b5624edea449dff6001fa387262
tomntoms-min <tomntoms@korea.ac.kr>
2025-06-11 22:00:01 +0900

    Update tcphdr.h

--- tcphdr.h
+++ tcphdr.h
@@ -1,21 +1,23 @@
 #pragma once
 
-#include <cstdint>
-#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #pragma pack(push, 1)
-struct TcpHdr {
-    uint16_t th_sport;
-    uint16_t th_dport;
-    uint32_t th_seq;
-    uint32_t th_ack;
-    uint8_t  th_offx2;
-    uint8_t  th_flags;
-    uint16_t th_win;
-    uint16_t th_sum;
-    uint16_t th_urp;
 
-    uint8_t th_off() const { return (th_offx2 & 0xF0) >> 4; }
+struct TcpHdr final {
+    uint16_t th_sport;       /* source port */
+    uint16_t th_dport;       /* destination port */
+    uint32_t th_seq;         /* sequence number */
+    uint32_t th_ack;         /* acknowledgement number */
+
+    // '정답 코드'의 비트 필드 방식을 적용하여 수정
+    uint8_t  th_x2:4,        /* (unused) */
+             th_off:4;       /* data offset */
+
+    uint8_t  th_flags;       /* control flags */
+    uint16_t th_win;         /* window */
+    uint16_t th_sum;         /* checksum */
+    uint16_t th_urp;         /* urgent pointer */
 
     enum: uint8_t {
         FIN = 0x01,
@@ -23,7 +25,7 @@ struct TcpHdr {
         RST = 0x04,
         PSH = 0x08,
         ACK = 0x10,
-        URG = 0x20,
+        URG = 0x20
     };
 };
 typedef TcpHdr *pTcpHdr;
